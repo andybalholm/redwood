4cdc10b3b672330ab6cd8ddcc740b1dce41dbaaa
Andy Balholm <andy@balholm.com>
2011-09-20 09:12:32 -0700

    Add CPU profiling support.

--- redwood.go
+++ redwood.go
@@ -6,13 +6,27 @@ package main
 import (
 	"flag"
 	"go-icap.googlecode.com/hg"
+	"log"
+	"os"
+	"runtime/pprof"
 )
 
 var configFile = flag.String("c", "/etc/redwood/redwood.conf", "configuration file path")
 var testURL = flag.String("test", "", "URL to test instead of running ICAP server")
+var cpuProfile = flag.String("cpuprofile", "", "write cpu profile to file")
 
 func main() {
 	flag.Parse()
+
+	if *cpuProfile != "" {
+		f, err := os.Create(*cpuProfile)
+		if err != nil {
+			log.Fatal(err)
+		}
+		pprof.StartCPUProfile(f)
+		defer pprof.StopCPUProfile()
+	}
+
 	loadConfiguration()
 
 	if *testURL != "" {

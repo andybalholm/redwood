42ea7ff5cf72dce5c9457d28afb3eaf9e010e017
Andy Balholm <andy@balholm.com>
2012-10-18 11:07:58 -0700

    Include the scheme with the hostname.

--- url.go
+++ url.go
@@ -121,7 +121,7 @@ func (m *URLMatcher) AddRule(r rule) {
 func (m *URLMatcher) MatchingRules(u *url.URL) map[rule]int {
 	result := make(map[rule]int)
 
-	host := strings.ToLower(u.Host)
+	host := u.Host
 	path := strings.ToLower(u.Path)
 
 	// strip off the port number, if present
@@ -129,6 +129,7 @@ func (m *URLMatcher) MatchingRules(u *url.URL) map[rule]int {
 	if colon != -1 {
 		host = host[:colon]
 	}
+	host = strings.ToLower(u.Scheme + "://" + host)
 
 	m.regexes.findMatches(strings.ToLower(u.String()), result)
 	m.hostRegexes.findMatches(host, result)

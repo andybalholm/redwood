623094d73e726b7ff7b272e613015d9e67e062ad
Andy Balholm <andy@balholm.com>
2013-07-13 16:20:58 -0700

    Add {{.Group}} variable to block page.
    Also document more-advanced template usage on block page, and add
    eq function.

--- blockpage.go
+++ blockpage.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"fmt"
 	"html/template"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"strings"
@@ -19,10 +20,20 @@ var blockTemplate *template.Template
 const transparent1x1 = "GIF89a\x10\x00\x10\x00\x80\xff\x00\xc0\xc0\xc0\x00\x00\x00!\xf9\x04\x01\x00\x00\x00\x00,\x00\x00\x00\x00\x10\x00\x10\x00\x00\x02\x0e\x84\x8f\xa9\xcb\xed\x0f\xa3\x9c\xb4\u068b\xb3>\x05\x00;"
 
 func loadBlockPage() {
-	var err error
-	blockTemplate, err = template.ParseFiles(*blockPage)
+	blockTemplate = template.New("blockpage")
+	content, err := ioutil.ReadFile(*blockPage)
 	if err != nil {
-		log.Println("Could not load block page template:", err)
+		log.Println("error loading block page template:", err)
+		return
+	}
+	blockTemplate.Funcs(template.FuncMap{
+		"eq": func(a, b interface{}) bool {
+			return a == b
+		},
+	})
+	_, err = blockTemplate.Parse(string(content))
+	if err != nil {
+		log.Println("error parsing block page template:", err)
 	}
 }
 
@@ -30,6 +41,7 @@ type blockData struct {
 	URL        string
 	Categories string
 	User       string
+	Group      string
 	Tally      string
 	Scores     string
 }
@@ -52,6 +64,7 @@ func showBlockPage(w http.ResponseWriter, r *http.Request, sc *scorecard, user s
 		URL:        r.URL.String(),
 		Categories: strings.Join(blockDesc, ", "),
 		User:       user,
+		Group:      whichGroup[user],
 		Tally:      listTally(stringTally(sc.tally)),
 		Scores:     listTally(sc.scores),
 	}

6d74168c2da3913fa40dc0762c984f62fe860a53
Andy Balholm <andy@balholm.com>
2013-10-15 14:45:09 -0700

    Provide transparent filtering of HTTPS hostnames, even without cert installed.

--- transparent.go
+++ transparent.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"net"
+	"net/url"
 	"time"
 )
 
@@ -42,8 +43,52 @@ func runTransparentServer(addr string) error {
 			log.Println("Error getting original address for intercepted connection:", err)
 			continue
 		}
-		go SSLBump(conn, serverAddr, "")
+		if tlsReady {
+			go SSLBump(conn, serverAddr, "")
+		} else {
+			go checkSSLHost(conn, serverAddr)
+		}
 	}
 
 	panic("unreachable")
+}
+
+// checkSSLHost does hostname filtering on an SSL connection when full ssl
+// filtering can't be done because no root certificate is installed.
+func checkSSLHost(conn net.Conn, serverAddr string) {
+	// Read the client hello so that we can find out the name of the server (not
+	// just the address).
+	clientHello, err := readClientHello(conn)
+	if err != nil {
+		log.Printf("error reading client hello in TLS connection from %s to %s: %s", conn.RemoteAddr(), serverAddr, err)
+		connectDirect(conn, serverAddr, clientHello)
+		return
+	}
+
+	serverName, ok := clientHelloServerName(clientHello)
+	if ok && serverName != "" {
+		if *tlsVerbose {
+			log.Printf("Server name requested for %s is %s.", serverAddr, serverName)
+		}
+	} else {
+		serverName = serverNameAtAddress(serverAddr)
+		if *tlsVerbose {
+			log.Printf("Server name detected at %s is %s.", serverAddr, serverName)
+		}
+	}
+
+	sc := scorecard{
+		tally: URLRules.MatchingRules(&url.URL{
+			Scheme: "https",
+			Host:   serverName,
+		}),
+	}
+	sc.calculate("")
+	if sc.action == BLOCK {
+		log.Println("Blocked HTTPS connection to", serverName)
+		conn.Close()
+		return
+	}
+
+	connectDirect(conn, serverAddr, clientHello)
 }

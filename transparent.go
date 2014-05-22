99e1ee4efc9be7a537a7e15b6a8f3077cbdf5e1e
Andy Balholm <andy@balholm.com>
2014-05-22 09:23:04 -0700

    Prevent infinite redirect loops.
    When Redwood was doing transparent HTTPS filtering, if someone made a TCP
    connection directly to the transparent port (instead of being redirected
    to it by the firewall), it would cause an infinite loop of redirected
    connections. Now it detects that and closes the connection.

--- transparent.go
+++ transparent.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"log"
 	"net"
 	"time"
@@ -15,6 +16,12 @@ func runTransparentServer(addr string) error {
 		return err
 	}
 	listenerChan <- ln
+
+	localAddresses, err := getLocalAddresses()
+	if err != nil {
+		log.Println("Error getting list of this server's IP addresses:", err)
+		// Continue, but without protection against infinite redirect loops.
+	}
 
 	var tempDelay time.Duration
 
@@ -43,8 +50,50 @@ func runTransparentServer(addr string) error {
 			continue
 		}
 		user, _, _ := net.SplitHostPort(conn.RemoteAddr().String())
-		go SSLBump(conn, serverAddr, user)
+
+		var server string
+		if tcpAddr, ok := serverAddr.(*net.TCPAddr); ok {
+			server = tcpAddr.IP.String()
+		} else {
+			server = serverAddr.String()
+		}
+		if localAddresses[server] {
+			// This is not an intercepted connection; it is a direct connection to
+			// our transparent port. If we bump it, we will end up with an infinite
+			// loop of redirects.
+			logTLS(user, serverAddr.String(), "", errors.New("infinite redirect loop"))
+			conn.Close()
+			continue
+		}
+
+		go SSLBump(conn, serverAddr.String(), user)
 	}
 
 	panic("unreachable")
+}
+
+// getLocalAddresses returns a set of the IP addresses of this machine's
+// network interfaces.
+func getLocalAddresses() (map[string]bool, error) {
+	res := map[string]bool{}
+
+	ifs, err := net.Interfaces()
+	if err != nil {
+		return nil, err
+	}
+
+	for _, iface := range ifs {
+		addrs, err := iface.Addrs()
+		if err != nil {
+			return nil, err
+		}
+		for _, a := range addrs {
+			if ipNet, ok := a.(*net.IPNet); ok {
+				a = &net.IPAddr{IP: ipNet.IP}
+			}
+			res[a.String()] = true
+		}
+	}
+
+	return res, nil
 }

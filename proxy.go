6daf6ff6acf0ed51a5f1fbbef8e1c96d285870a2
Andy Balholm <andy@balholm.com>
2020-12-26 17:11:45 -0800

    Remove obsolete workaround for Mac App Store trailer issue.
    Apparently the App Store no longer has this problem, likely because they
    switched to HTTPS, and we need to sslbypass it anyway.

--- proxy.go
+++ proxy.go
@@ -327,12 +327,6 @@ func (h proxyHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	removeHopByHopHeaders(r.Header)
 	resp, err := rt.RoundTrip(r)
 
-	if err != nil && strings.Contains(err.Error(), "trailer header without chunked transfer encoding") && r.Method == "GET" && r.URL.Scheme == "http" {
-		// Use a simpler transport that will ignore the error.
-		log.Printf("Using simpleTransport for %v", r.URL)
-		resp, err = simpleTransport{}.RoundTrip(r)
-	}
-
 	if err == context.Canceled {
 		return
 	}
--- transport.go
+++ transport.go
@@ -5,16 +5,12 @@ import (
 	"context"
 	"crypto/tls"
 	"crypto/x509"
-	"fmt"
 	"io"
 	"log"
 	"mime"
 	"net"
 	"net/http"
-	"net/textproto"
 	"path"
-	"strconv"
-	"strings"
 	"sync"
 	"time"
 
@@ -82,97 +78,6 @@ func dialWithExtraRootCerts(network, addr string) (net.Conn, error) {
 
 var transportWithExtraRootCerts = &http.Transport{
 	DialTLS: dialWithExtraRootCerts,
-}
-
-// A simpleTransport fetches a single file over plain HTTP, as simply as
-// possible.
-type simpleTransport struct{}
-
-func (simpleTransport) RoundTrip(req *http.Request) (resp *http.Response, err error) {
-	if req.Method != "GET" {
-		return nil, fmt.Errorf("request method not supported by simpleTransport (expected GET, got %s)", req.Method)
-	}
-	if req.URL.Scheme != "http" {
-		return nil, fmt.Errorf("URL scheme not supported by simpleTransport (expected http, got %s)", req.URL.Scheme)
-	}
-
-	host := req.URL.Host
-	if _, _, err := net.SplitHostPort(host); err != nil {
-		host = net.JoinHostPort(host, "80")
-	}
-
-	conn, err := dialer.Dial("tcp", host)
-	if err != nil {
-		return nil, fmt.Errorf("error connecting to %s: %v", host, err)
-	}
-	if err = req.Write(conn); err != nil {
-		return nil, fmt.Errorf("error sending request for %v: %v", req.URL, err)
-	}
-
-	br := bufio.NewReader(conn)
-	tp := textproto.NewReader(br)
-	resp = &http.Response{
-		Request: req,
-	}
-
-	// Parse the first line of the response.
-	line, err := tp.ReadLine()
-	if err != nil {
-		if err == io.EOF {
-			err = io.ErrUnexpectedEOF
-		}
-		return nil, err
-	}
-	if i := strings.IndexByte(line, ' '); i == -1 {
-		return nil, fmt.Errorf("malformed HTTP response: %q", line)
-	} else {
-		resp.Proto = line[:i]
-		resp.Status = strings.TrimLeft(line[i+1:], " ")
-	}
-	statusCode := resp.Status
-	if i := strings.IndexByte(resp.Status, ' '); i != -1 {
-		statusCode = resp.Status[:i]
-	}
-	if len(statusCode) != 3 {
-		return nil, fmt.Errorf("malformed HTTP status code: %q", statusCode)
-	}
-	resp.StatusCode, err = strconv.Atoi(statusCode)
-	if err != nil || resp.StatusCode < 0 {
-		return nil, fmt.Errorf("malformed HTTP status code: %q", statusCode)
-	}
-	var ok bool
-	if resp.ProtoMajor, resp.ProtoMinor, ok = http.ParseHTTPVersion(resp.Proto); !ok {
-		return nil, fmt.Errorf("malformed HTTP version: %q", resp.Proto)
-	}
-
-	// Parse the response headers.
-	mimeHeader, err := tp.ReadMIMEHeader()
-	if err != nil {
-		if err == io.EOF {
-			err = io.ErrUnexpectedEOF
-		}
-		return nil, err
-	}
-	resp.Header = http.Header(mimeHeader)
-
-	cl := resp.Header.Get("Content-Length")
-	if cl != "" {
-		if resp.ContentLength, err = strconv.ParseInt(cl, 10, 64); err != nil {
-			return nil, fmt.Errorf("invalid Content-Length: %q", cl)
-		}
-	}
-	resp.Header.Del("Content-Length")
-	resp.Header.Del("Trailer")
-
-	var body struct {
-		io.Reader
-		io.Closer
-	}
-	body.Reader = &io.LimitedReader{br, resp.ContentLength}
-	body.Closer = conn
-	resp.Body = body
-
-	return resp, nil
 }
 
 // A connTransport is an http.RoundTripper that uses a single network

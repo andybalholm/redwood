8c4ad43ce7dc2d915f2641416371b21acc99e7a9
Eliezer Croitoru <ngtech1ltd@gmail.com>
2021-03-29 22:42:10 +0300

    Added CORS * and block page mark header

--- blockpage.go
+++ blockpage.go
@@ -83,6 +83,9 @@ func (c *config) aclDescriptions(rule ACLActionRule) []string {
 
 // showBlockPage shows a block page for a page that was blocked by an ACL.
 func showBlockPage(w http.ResponseWriter, r *http.Request, resp *http.Response, user string, tally map[rule]int, scores map[string]int, rule ACLActionRule) {
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("X-Redwood-Block-Page", "403 Access Denied")
+
 	c := getConfig()
 	switch {
 	case c.BlockTemplate != nil:

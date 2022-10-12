961c2c19d607cbe2a1c6dd89349afb95d5b36e2c
Andy Balholm <andy@balholm.com>
2022-10-12 10:46:13 -0700

    When sniffing Content-Type, set the Content-Type header.
    http.Server would have set the header anyway, but if we do it earlier,
    it will be logged in the access log.

--- proxy.go
+++ proxy.go
@@ -348,17 +348,6 @@ func (h proxyHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 	removeHopByHopHeaders(resp.Header)
 
-	// Yet another workaround for https://github.com/golang/go/issues/31753
-	if resp.Header.Get("Content-Type") == "" && resp.Header.Get("Content-Encoding") == "gzip" && r.Method != "HEAD" {
-		gzr, err := gzip.NewReader(resp.Body)
-		if err != nil {
-			log.Printf("Error creating gzip reader for %v: %v", r.URL, err)
-		} else {
-			resp.Body = gzr
-			resp.Header.Del("Content-Encoding")
-		}
-	}
-
 	response := &Response{
 		Request:  request,
 		Response: resp,
